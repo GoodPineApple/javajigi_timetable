298ae6cbe5dca77f68e7f2eb4a2b0f4284ce741d
Taemin Kim <taemin3000@naver.com>
2017-06-28 17:35:55 +0900

    ver02 using Scanner input

---
+++ ver02Better/scannerTimesTable.java
@@ -0,0 +1,24 @@
+package timesTable.ver02Better;
+
+import java.util.Scanner;
+
+/**
+ * Created by taemi on 2017-06-28.
+ */
+public class scannerTimesTable {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("출력할 단을 입력하시오 : ");
+        int input = scan.nextInt();
+        System.out.println(input * 1);
+        System.out.println(input * 2);
+        System.out.println(input * 3);
+        System.out.println(input * 4);
+        System.out.println(input * 5);
+        System.out.println(input * 6);
+        System.out.println(input * 7);
+        System.out.println(input * 8);
+        System.out.println(input * 9);
+
+    }
+}
