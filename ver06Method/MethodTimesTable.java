bedadc0d164860532cb939eea679d50b8e226f51
Taemin Kim <taemin3000@naver.com>
2017-06-28 18:23:48 +0900

    using method timetable

--- ver06Method/MethodTimesTable.java
+++ ver06Method/MethodTimesTable.java
@@ -4,4 +4,25 @@ package timesTable.ver06Method;
  * Created by taemi on 2017-06-28.
  */
 public class MethodTimesTable {
+
+    public static int[] genArray(int num){
+        int[] array = new int[9];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = num * (i+1);
+        }
+        return array;
+    }
+
+    public static void print(int[] times){
+        for (int i = 0; i < times.length; i++) {
+            System.out.println(times[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        for(int i=2; i<9; i++){
+            print(genArray((i)));
+        }
+    }
+
 }
