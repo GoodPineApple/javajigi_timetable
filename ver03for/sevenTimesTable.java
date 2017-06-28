e03ac3f8203408bed8493ac339c0642480c5b109
Taemin Kim <taemin3000@naver.com>
2017-06-28 17:43:23 +0900

    using for statement

---
+++ ver03for/sevenTimesTable.java
@@ -0,0 +1,12 @@
+package timesTable.ver03for;
+
+/**
+ * Created by taemi on 2017-06-28.
+ */
+public class sevenTimesTable {
+    public static void main(String[] args) {
+        for(int i=1; i<10; i++){
+            System.out.println(7*i);
+        }
+    }
+}
---
+++ ver03for/sixTimesTable.java
@@ -0,0 +1,14 @@
+package timesTable.ver03for;
+
+/**
+ * Created by taemi on 2017-06-28.
+ */
+public class sixTimesTable {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<10){
+            System.out.println(6 * i);
+            i++;
+        }
+    }
+}
