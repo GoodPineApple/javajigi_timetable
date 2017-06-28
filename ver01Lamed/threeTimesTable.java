d5f374ae53c130595eaa888e4e2985a7fba8a023
Taemin Kim <taemin3000@naver.com>
2017-06-28 17:26:56 +0900

    make folder javajigiStudy timetable

---
+++ ver01Lamed/threeTimesTable.java
@@ -0,0 +1,18 @@
+package timesTable.ver01Lamed;
+
+/**
+ * Created by taemi on 2017-06-28.
+ */
+public class threeTimesTable {
+    public static void main(String[] args) {
+        System.out.println(3*1);
+        System.out.println(3*2);
+        System.out.println(3*3);
+        System.out.println(3*4);
+        System.out.println(3*5);
+        System.out.println(3*6);
+        System.out.println(3*7);
+        System.out.println(3*8);
+        System.out.println(3*9);
+    }
+}
---
+++ ver01Lamed/twoTimesTable.java
@@ -0,0 +1,17 @@
+package timesTable.ver01Lamed;
+
+/**
+ * Created by taemi on 2017-06-28.
+ */
+public class twoTimesTable {
+    public static void main(String[] args) {
+        System.out.println(2*1);
+        System.out.println(2*2);
+        System.out.println(2*3);
+        System.out.println(2*5);
+        System.out.println(2*6);
+        System.out.println(2*7);
+        System.out.println(2*8);
+        System.out.println(2*9);
+    }
+}
